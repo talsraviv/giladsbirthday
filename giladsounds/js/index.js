9ff45119404e2a88f7cd1c16ee619535c2e3169d
talsraviv <tal@patreon.com>
2018-02-12 21:50:20 -0800

    INTERNET

--- giladsounds/js/index.js
+++ giladsounds/js/index.js
@@ -1,13 +1,24 @@
-var choke = document.getElementById("brody-choke");
-var chokeSound = document.querySelector("audio[data-key=\"choking\"]");
 
-var eating = document.getElementById("brody-eating");
-var eatingSound = document.querySelector("audio[data-key=\"eating\"]");
+document.getElementById("id1").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound1\"]").play();}, false);
+document.getElementById("id2").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound2\"]").play();}, false);
+document.getElementById("id3").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound3\"]").play();}, false);
+document.getElementById("id4").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound4\"]").play();}, false);
+document.getElementById("id5").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound5\"]").play();}, false);
+document.getElementById("id6").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound6\"]").play();}, false);
+document.getElementById("id7").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound7\"]").play();}, false);
+document.getElementById("id8").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound8\"]").play();}, false);
+document.getElementById("id9").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound9\"]").play();}, false);
+document.getElementById("id10").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound10\"]").play();}, false);
+document.getElementById("id12").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound11\"]").play();}, false);
+document.getElementById("id12").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound12\"]").play();}, false);
+document.getElementById("id13").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound13\"]").play();}, false);
+document.getElementById("id14").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound14\"]").play();}, false);
+document.getElementById("id15").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound15\"]").play();}, false);
+document.getElementById("id16").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound16\"]").play();}, false);
+document.getElementById("id17").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound17\"]").play();}, false);
+document.getElementById("id18").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound18\"]").play();}, false);
+document.getElementById("id19").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound19\"]").play();}, false);
+document.getElementById("id20").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound20\"]").play();}, false);
+document.getElementById("id21").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound21\"]").play();}, false);
+document.getElementById("id22").addEventListener("click", function (event) {document.querySelector("audio[data-key=\"sound22\"]").play();}, false);
 
-choke.addEventListener("click", function (event) {
-  chokeSound.play();
-}, false);
-
-eating.addEventListener("click", function (event) {
-  eatingSound.play();
-}, false);
